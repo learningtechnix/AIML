24170b54feff1fcaedac69dde49e946877040d90
learningtechnix <64873197+learningtechnix@users.noreply.github.com>
2023-11-05 18:58:42 +0530

    Update shopping_app.py

--- shopping_app.py
+++ shopping_app.py
@@ -1,11 +1,12 @@
 from getpass import getpass
 import uuid
 import random
+import time
 
 
 pre_defined_users = {
-    'user1': {'password': 'password1', 'role': 'user', 'sessionId': '900001'},
-    'admin1': {'password': 'password2', 'role': 'admin', 'sessionId': '100001'},
+    'user1': {'password': 'user1', 'role': 'user'},
+    'admin1': {'password': 'admin1', 'role': 'admin'},
             }
 
 products = [
@@ -24,11 +25,12 @@ shopping_cart = {}
 def display_users():
     print()
     print(50*"-")
-    print("Username\tRole\tSessionID")
+    print("Username\tRole")
     print(50*"-")
     for username, user_info in pre_defined_users.items():
-        print(f"{username}\t{user_info['role']}\t{user_info['sessionId']}")
+        print(f"{username}\t{user_info['role']}")
     print()
+    time.sleep(1)
 
 def create_user():
     session_id = str(random.randint(100000, 999999))
@@ -39,7 +41,6 @@ def create_user():
             'sessionId': session_id
             }
     return new_user_info
-    #pre_defined_users[new_user_info['username']]
 
 def delete_user():
     user_to_delete = input("Enter the username to delete: ")
@@ -52,15 +53,21 @@ def delete_user():
 
 def display_shopping_catalog():
     print()
+    print(50*"-")
     print("Product ID\tName\tCategory ID\tPrice")
     print(50*"-")
     print()
     for product_catalogue in products:
             print(f"{product_catalogue['id']}\t{product_catalogue['name']}\t{product_catalogue['Category']}\t{product_catalogue['price']}")
+    print(50*"-")
 
 def add_item_to_catalogue():
     selection = input("Looking to add an Item to Existing category (y|n): ")
     if selection == 'y':
+        print()
+        print("Displaying existing catalogue for reference")
+        display_shopping_catalog()
+        print()
         update_product_catalogue = {
                 'Category': input("Provide category Name: "),
                 'name': input("Item name: "),
@@ -78,14 +85,33 @@ def add_item_to_catalogue():
                 }
         return update_product_catalogue
 
+def del_item_from_catalogue():
+    selection = int(input("Kindly share the Product ID of item to delete: "))
+    for product in products:
+        if product['id'] == selection:
+            products.remove(product)
+            print()
+            print(f"Item {product['name']} with Product ID {product['id']} is successfully removed from {product['Category']} category.")
+            time.sleep(2)
+            print("Below is the updated catalogue")
+            time.sleep(1)
+            display_shopping_catalog()
 def view_cart(cart_user):
     if not shopping_cart:
-        print("Cart is Empty")
+        print(40*"*")
+        print("Cart is Empty. Feel free to add items")
+        print(40*"*")
     else:
         print()
+        print(50*"*")
+        print(50*"*")
         print(f"Shopping cart of {cart_user}")
+        print(50*"*")
+        print(50*"*")
+        print(f"Id\tName\tQuantity\tItem Price\tTotal Cost")
         for username, cart_items in shopping_cart.items():
-            print(f"{cart_items['Product Id']}\t{cart_items['Item Name']}\t{cart_items['Quantity']}\t{cart_items['Cost']}")
+            print(f"{cart_items['Product Id']}.\t{cart_items['Item Name']}\t{cart_items['Quantity']}\t{cart_items['Price']}\t{cart_items['Cost']}")
+        print(50*"*")
 
 def add_to_cart():
     print()
@@ -103,9 +129,13 @@ def add_to_cart():
                     'Product Id': product_catalogue['id'],
                     'Item Name': product_catalogue['name'],
                     'Quantity': quantity,
+                    'Price': product_catalogue['price'],
                     'Cost': total_cost
                     }
-            print("Items added successfully to cart")
+            print(50*"*")
+            print(f"Item {product_catalogue['name']} is  added successfully to cart")
+            print(50*"*")
+            time.sleep(2)
         else:
             pass
     return temporary_shopping_cart
@@ -131,9 +161,14 @@ def make_payment():
     print("\t4. UPI")
     print("\t5. Pay on Delivery")
     choice = int(input("Enter Choice: "))
+    print()
     if choice in range(1,6):
         for username, cart_items in shopping_cart.items():
+            time.sleep(2)
+            print(50*"*")
             print(f"You will be shortly redirected to the portal for Unified Payment Interface to make a payment of Rs. {cart_items['Cost']}")
+            print(50*"*")
+            time.sleep(1)
     else:
         print("Invalid Option, Exiting ...")
 
@@ -145,23 +180,26 @@ def user_login():
     if loginname in pre_defined_users:
         if loginpass == pre_defined_users[loginname]['password']:
             if pre_defined_users[loginname]['role'] == 'user':
+                session_id = str(random.randint(900000, 999999))
                 while True:
                     print()
-                    print("Welcome {0}! You logged in as {1} role. Below are options available for you.".format(loginname, pre_defined_users[loginname]['role']))   
-                    print("What operations you are looking to perform ?")
+                    print(100*"-")
+                    print("Welcome {0}! You logged in as \"{1}\" role with sessionID {2}. Kindly choose from below:".format(loginname, pre_defined_users[loginname]['role'], session_id))
+                    print(100*"-")
                     print()
                     print("1. View Items into your cart")
                     print("2. Adding Items to cart")
                     print("3. Removing Items from cart")
                     print("4. Making a payment")
-                    print("9. Back to main menu")
+                    print("10. Back to main menu")
                     print()
                     choice = input("Select option: ")
-                    if choice == '9':
+                    if choice == '10':
                         welcome_msg()
                         break
                     elif choice == '1':
                         view_cart(loginname)
+                        time.sleep(1)
                     elif choice == '2':
                         shopping_cart_temp = add_to_cart()
                         shopping_cart[shopping_cart_temp['username']] = shopping_cart_temp
@@ -173,23 +211,28 @@ def user_login():
                         print("Undefined option, Exiting ...")
                         break
             else:
+                session_id = str(random.randint(100000, 199999))
                 while True:
                     print()
                     print("Welcome {0}! You logged in as {1} role. Below are options available for you.".format(loginname, pre_defined_users[loginname]['role']))   
                     print("What operations you are looking to perform ?")
                     print()
+                    print(10*"*"," User Role specific options ",10*"*")
                     print("1. View Items into your cart")
                     print("2. Adding Items to cart")
                     print("3. Removing Items from cart")
-                    print("4. Making a payment")
+                    print(30*"*")
+                    print()
+                    print("4. Displaying catalogue")
                     print("5. Displaying current users")
                     print("6. Create new users")
                     print("7. Delete a user")
                     print("8. Add items to catalogue")
-                    print("9. Back to main menu")
+                    print("9. Removing item from catalogue")
+                    print("10. Back to main menu")
                     print()
                     choice = input("Select option: ")
-                    if choice == '9':
+                    if choice == '10':
                         welcome_msg()
                         break
                     elif choice == '5':
@@ -204,9 +247,14 @@ def user_login():
                         products.append(updated_catalogue)
                         print("Displaying updating catalogue")
                         display_shopping_catalog()
+                    elif choice == '4':
+                        display_shopping_catalog()
+                        time.sleep(2)
+                    elif choice == '9':
+                        del_item_from_catalogue()
                     else:
                         print("Being admin you are not AUTHORIZED to perform this operation , Try again ...")
-                        break
+                        time.sleep(1)
         else:
             print("Login Failed ! {0}, please validate your credentials".format(loginname))
     else:
@@ -217,7 +265,7 @@ def welcome_msg():
         print()
         print("Welcome to the Demo Marketplace")
         print("*"*30)
-        print("For ease of operation, we have provided 2 predefined user, \n'user1' is normal user with password 'password1', \n'admin1' is admin user with password 'password2'")
+        print("For ease of operation, we have provided 2 predefined user, \n'user1' is normal user with password 'user1', \n'admin1' is admin user with password 'admin1'")
         print("*"*50)
         print()
         print("1. Display shopping catalogue")
